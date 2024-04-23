c1600105b337c00592fffd7dbb0cad107cafe164
dustin <dustinshimono@gmail.com>
2024-04-23 08:25:45 -0400

    chore: ollama wip

---
+++ src/main/java/com/dustin/ai/rag/myrag/ollama/OllamaController.java
@@ -0,0 +1,35 @@
+package com.dustin.ai.rag.myrag.ollama;
+
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.*;
+
+@RestController
+public class OllamaController {
+
+    private final OllamaChatClient chatClient;
+
+    @Autowired
+    public OllamaController(OllamaChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/ai/ollama/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatClient.call(message));
+    }
+
+    @GetMapping("/ai/ollama/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatClient.stream(prompt);
+    }
+}
